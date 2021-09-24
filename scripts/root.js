73a264fe265070a2ce0cb556735e4d8769639642
Rahul Gupta <notsonoobiee@gmail.com>
2021-09-24 13:06:41 +0530

    Serverless - HTTP Boilerplate setup

---
+++ scripts/root.js
@@ -0,0 +1,13 @@
+module.exports.handler = async (event) => (
+  {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Hello World',
+        input: event,
+      },
+      null,
+      2,
+    ),
+  }
+);
